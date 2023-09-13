1bcec1a77d838f423c237b6e33b21d910e562113
kimyohan <rladygks1210@naver.com>
2023-09-13 16:42:18 +0900

    입력예제 내부클래스

--- javawork/NaverJavaProject/src/bit701/day0913/Ex8_InnerClass.java
+++ javawork/NaverJavaProject/src/bit701/day0913/Ex8_InnerClass.java
@@ -17,10 +17,25 @@ class OuterA {
 
 		}
 	}
+	
+	static class InnerB{
+		int e = 50;
+		static int f=60;
+		public void showInnerB() {
+			System.out.println("** static 내부 클래스의 메서드 **");
+			//System.out.println("외부 a=" +a); //외부의 일반 인스턴스 멤버변수 접근 불가능
+			System.out.println("내부 b=" + b);
+			System.out.println("내부 e=" + e);
+			System.out.println("내부 f=" + f);
+		}
+	}
+	
 	//외부 클래스의 메서드
 	public void showOuter() {
 		InnerA inA = new InnerA();
 		inA.showInner();
+		InnerB inB = new InnerB();
+		inB.showInnerB();
 	}
 }
 
@@ -35,9 +50,13 @@ public class Ex8_InnerClass {
 		a.showOuter();
 		System.out.println();
 		//다른 클래스에서 직접 내부클래스의 메서드를 호출하고 싶을경우
-		OuterA.InnerA b = new OuterA(). new InnerA();
+//		OuterA.InnerA b = new OuterA(). new InnerA();
+		OuterA.InnerA b = a.new InnerA();
 		b.showInner();
-		
+		System.out.println("---------");
+		//static 내부 클래스를 직접 선언하여 메서드를 호출하고자 할 경우
+		OuterA.InnerB c = new OuterA.InnerB();
+		c.showInnerB();
 	}
 
 }
