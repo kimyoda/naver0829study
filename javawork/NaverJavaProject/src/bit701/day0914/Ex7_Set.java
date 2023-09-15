d35aee1a3e517f5c38548a39098c66d5a1fe2ed8
kimyohan <rladygks1210@naver.com>
2023-09-15 10:03:22 +0900

    어제 공부한 내용 확인

--- javawork/NaverJavaProject/src/bit701/day0914/Ex7_Set.java
+++ javawork/NaverJavaProject/src/bit701/day0914/Ex7_Set.java
@@ -1,16 +1,16 @@
 package bit701.day0914;
 
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Ex7_Set {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Set 인터페이스의 특징 : 1. 순차적이 아니다. 2. 중복허용을 안한다
-		Set<Integer> set = new HashSet<Integer>();//비순차적, 주로사용한다
-//		Set<Integer> set = new TreeSet<Integer>();//오름차순정렬되서 들어감
+//		Set<Integer> set = new HashSet<Integer>();//비순차적, 주로사용한다
+		Set<Integer> set = new TreeSet<Integer>();//오름차순정렬되서 들어감
 		System.out.println(set.size());//size는 데이타의 갯수(배열은 length)
 		set.add(30);
 		set.add(1);
--- javawork/NaverJavaProject/src/bit701/day0914/Ex8_Set.java
+++ javawork/NaverJavaProject/src/bit701/day0914/Ex8_Set.java
@@ -1,14 +1,14 @@
 package bit701.day0914;
 
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Ex8_Set {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Set<String> set = new HashSet<String>();//비순차적 
-		Set<String> set = new TreeSet<String>();//순차적, 순서대로 나온다
+		Set<String> set = new HashSet<String>();//비순차적 
+//		Set<String> set = new TreeSet<String>();//순차적, 순서대로 나온다
 		set.add("red");
 		set.add("blue");
 		set.add("green");
