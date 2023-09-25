d5df2d7d7c9491703ec15015cc92d34aa3dffd7d
kimyohan <rladygks1210@naver.com>
2023-09-25 10:29:13 +0900

    Orcale, Mysql 연결

---
+++ javawork/NaverJavaProject/src/bit701/day0925/Ex1_OrcaleShopInsert.java
@@ -0,0 +1,19 @@
+package bit701.day0925;
+
+import bit701.day0922.Ex1_OracleJdbc;
+import db.DbConnect;
+
+public class Ex1_OrcaleShopInsert {
+	DbConnect db = new DbConnect();
+	
+	public void shopInsert() {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ex1_OrcaleShopInsert ex = new Ex1_OrcaleShopInsert();
+		ex.shopInsert();
+	}
+
+}
