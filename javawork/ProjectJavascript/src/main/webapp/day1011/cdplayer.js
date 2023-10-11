30a08271abad98a5e54af2ddd85d10d1bedb0f46
kimyohan <rladygks1210@naver.com>
2023-10-11 17:31:04 +0900

    "css, cd 예제, 애니메이션"

---
+++ javawork/ProjectJavascript/src/main/webapp/day1011/cdplayer.js
@@ -0,0 +1,27 @@
+$(function(){
+	// h1, title 을 클릭시 .music-list를 slideDown 한다
+	$("h1.title").click(function(){
+		$("ul.music-list").slideDown('slow');
+	});
+	
+	
+	// 노래 제목을 클릭 시 클릭한 곳의 제목을 얻어서 h1.title에 넣은 후 slideUp한다.
+	$("ul.music-list li").click(function(){
+		// 노래제목
+		let title=$(this).text();
+		// 적용
+		$("h1.title").text(title);
+		// class 를 얻는다
+		let selectClass=$(this).attr("class");
+		
+		// 적용-animate
+		$("#cd").animate({width:'0'}, 'slow',function(){
+			// cd 변경
+			$("#cd").attr("class", selectClass);
+			// 너비를 다시 300으로 변경
+			$(this).animate({width: '300px'}, 'slow');
+		});
+		
+		$("ul.music-list").slideUp('slow');
+	});
+ });
