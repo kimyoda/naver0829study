fccb855e48f44f57542010b7f309e4ea106df8b9
kimyohan <rladygks1210@naver.com>
2023-11-14 20:30:41 +0900

    [add] 20231104 프로젝트 세팅하기

---
+++ springwork/SpringMysqlBoardEx7/src/main/java/board/controller/BoardListController.java
@@ -0,0 +1,25 @@
+package board.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import board.data.BoardDao;
+
+@Controller
+//@AllArgsConstructor
+public class BoardListController {
+    @Autowired
+	private BoardDao boardDao;
+	
+	@GetMapping("/simple/list")
+	public String list(Model model) {
+		// 전체 갯수 가져오기
+		int totalCount = boardDao.getTotalCount();
+		
+		// model에 저장
+		model.addAttribute("totalCount", totalCount);
+		return "list";
+	}
+}
---
+++ springwork/SpringUploadMVCEx6/src/main/java/study/spring/day1114/PhotoUploadController.java
@@ -0,0 +1,107 @@
+package study.spring.day1114;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import data.dto.MemberDto;
+
+@Controller
+public class PhotoUploadController {
+   
+   @PostMapping("/member/upload1")
+   public String upload1(@ModelAttribute("mdto") MemberDto dto, @RequestParam MultipartFile upload, 
+         HttpServletRequest request) {
+      
+      // 업로드할 프로젝트 내의 경로 구하기
+      String path = request.getSession().getServletContext().getRealPath("/WEB-INF/photo");
+      System.out.println(path);
+      
+      // 파일명을 dto 에 저장
+      // 파일명은 랜덤하게 만들어보자
+      String photo = UUID.randomUUID().toString();
+      dto.setPhoto(photo);
+      
+      // 업로드
+      try {
+         upload.transferTo(new File(path + "/" + photo));
+      } catch (IllegalStateException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      } catch (IOException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      
+      return "member/result1";
+   }
+   
+   @PostMapping("/member/upload2")
+   public String upload2(@ModelAttribute("mdto") MemberDto dto, HttpServletRequest request,
+		   @RequestParam ArrayList<MultipartFile> upload) {
+      
+	  String path = request.getSession().getServletContext().getRealPath("/WEB-INF/photo");
+	  // 파일명을 저장할 변수
+	  String photo = "";
+	  
+	  // 여러장의 사진을 업로드해야하므로 반복문을 돌린다
+	  for (MultipartFile m:upload) {
+		  // 임의의 파일을 얻는다
+		  String fname = UUID.randomUUID().toString();
+		  photo += fname + ",";
+		  
+		  // 업로드
+		  try {
+			m.transferTo(new File(path+ "/" + fname));
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	  }
+	  
+	  // 마지막 컴마 제거
+	  photo = photo.substring(0, photo.length()-1);
+	  // dto에 넣기
+	  dto.setPhoto(photo);
+	  return "member/result2";
+   }
+   
+   @PostMapping("/member/upload3")
+   public String upload3(@ModelAttribute("mdto") MemberDto dto, HttpServletRequest request, 
+		   @RequestParam MultipartFile upload, @RequestParam("upload2") MultipartFile upload2) {
+	   
+	   String path = request.getSession().getServletContext().getRealPath("/WEB-INF/photo");
+	   
+	   String photo1 = UUID.randomUUID().toString();
+	   String photo2 = UUID.randomUUID().toString();
+	   dto.setPhoto(photo1);
+	   dto.setPhoto2(photo2);
+	   
+       try {
+		upload.transferTo(new File(path + "/" + photo1));
+		upload2.transferTo(new File(path + "/" + photo2));
+	} catch (IllegalStateException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	   
+	   return "member/result3";
+   }
+   
+   
+}
